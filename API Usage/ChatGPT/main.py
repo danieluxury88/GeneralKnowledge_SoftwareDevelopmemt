16791a86b4b7e30097ea5ec4a705c72e73c8f78f
Daniel Proaño Chacón <danieluxury@gmail.com>
2023-03-14 19:08:19 -0500

    Improve menu - Create basic structure.. to refactor.

--- API Usage/ChatGPT/main.py
+++ API Usage/ChatGPT/main.py
@@ -10,12 +10,13 @@ import openai
 class ChatGPTHandler:
 
     chat_role = "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Knowledge cutoff: January 2022 Current date: March 2023"
-
+    request_type =""
     def __init__(self):
         filename = None
         data = None
         user_request_index = 0
         max_tokens = 300
+
 
     def open_json_file(self, filename):
         self.filename = filename
@@ -35,13 +36,18 @@ class ChatGPTHandler:
     def look_for_api_key(self):
         api_key = os.environ.get("GPT_API_KEY")
         if api_key:
-            print(f"The API key is: {api_key}")
+            print(f"The API validated")
             openai.api_key = api_key
             return True
         else:
             print("The API key is not set.")
             # return False
             exit(1)
+
+    def get_request_type(self):
+        if self.request_type == "endulzar":
+           return "puedes endulzar esta frase: "
+        return self.request_type
 
     def ask_chat_gpt(self, question):
         user_request = {
@@ -56,14 +62,14 @@ class ChatGPTHandler:
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": primer},
-                {"role": "user", "content": question},
+                {"role": "user", "content": self.request_type + ' ' + question},
                 # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
                 # {"role": "user", "content": "Where was it played?"}
                 # {"role": "assistant", "content": "One example is edx CS50."},
             ],
         )
 
-        query = {"user_request": user_request, "chatgpt_response": chatgpt_reponse}
+        query = {"option": self.request_type, "user_request": user_request, "chatgpt_response": chatgpt_reponse}
         new_data = {"query": query}
         self.data.append(new_data)
         self.save_json_file()
@@ -99,6 +105,8 @@ class ChatGPTHandler:
             option = input("Enter a question or 'quit' to exit: ")
             if option in ["quit", "exit"]:
                 break
+            elif option in ["endulzar"]:
+                self.request_type = "endulzar"
             elif option == "clear":
                 os.system("clear")
                 os.system("clswhat")
@@ -122,6 +130,127 @@ class ChatGPTHandler:
                 self.log_last_query_tokens_usage()
 
 
+    def execute(self, question):
+        self.look_for_api_key()
+        self.open_json_file("data.json")
+        self.ask_chat_gpt(question)
+        question = self.get_last_question()
+        response = self.get_last_response()
+        response_md = self.format_as_markdown(response)
+
+        print(question, end="\n\n")
+        print(response_md, end="\n\n")
+        self.log_last_query_tokens_usage()
+
+    def configure(self, option):
+        if option =='e':
+            self.request_type = "endulzar"
+        elif option =='d':
+            self.request_type = ""
+
+
+
+
+# <h1>Define the nested menus and their options</h1>
+
+
+class CLIMenuHandler:
+
+    # Define a function to display a menu and get user input</h1>
+    def __init__(self):
+        app = None
+        self.MAIN_MENU = {
+            ' ': 'Question',
+            '1': 'Option 1',
+            '2': 'Option 2',
+            '3': 'Nested Options',
+            'c': 'Clear',
+            'o': 'Fast Options',
+            '0': 'Exit'
+        }
+
+        self.NESTED_MENU_1 = {
+            '1': 'Nested Option 1',
+            '2': 'Nested Option 2',
+            '0': 'Back'
+        }
+
+        self.NESTED_MENU_2 = {
+            '1': 'Nested Option 3',
+            '2': 'Nested Option 4',
+            '0': 'Back'
+        }
+
+        self.NESTED_MENUS = {
+            '1': self.NESTED_MENU_1,
+            '2': self.NESTED_MENU_2,
+            '0': 'Back'
+        }
+
+
+
+        self.FAST_OPTIONS_MENUS = {
+            'e': 'endulzar',
+            'd': 'default mode',
+            '0': 'Back',
+        }
+
+
+    def set_app(self, app):
+        self.app = app
+        
+
+    def get_user_choice(self, menu):
+        for key, value in menu.items():
+            print(f'{key}: {value}')
+        choice = input('Please enter your choice: ')
+        return choice
+
+
+
+    # Define a function to execute the selected menu option+
+    def execute_choice(self, choice):
+        if choice == '1':
+            print('Selected option 1.')
+        elif choice == '2':
+            print('Selected option 2.')
+        elif choice == '3':
+            nested_choice = ''
+            while nested_choice != '0':
+                nested_choice = self.get_user_choice(self.NESTED_MENUS)
+                if (nested_choice == 0):
+                    return
+                self.execute_choice(nested_choice)
+        elif choice == '0':
+            print('Exiting')
+            exit()
+        elif choice == 'c':
+            os.system("clear")
+            os.system("clswhat")
+        elif choice == 'o':
+            nested_choice = ''
+            while nested_choice != '0':
+                nested_choice = self.get_user_choice(self.FAST_OPTIONS_MENUS)
+                if (nested_choice == '0'):
+                    return
+                elif nested_choice == 'e':
+                    self.app.configure('e')
+                    return
+            self.execute_choice(nested_choice)
+        else:
+            question = choice
+            self.app.execute(question)
+
+
+    def run(self):
+        while(True):
+            choice = self.get_user_choice(self.MAIN_MENU)
+            self.execute_choice(choice)
+
+
 if __name__ == "__main__":
-    handler = ChatGPTHandler()
-    handler.run()
+    menu_handler = CLIMenuHandler()
+    chat_gpt_app = ChatGPTHandler()
+    menu_handler.set_app(chat_gpt_app)
+    menu_handler.run()
+    
--- API Usage/ChatGPT/test.py
+++ API Usage/ChatGPT/test.py
@@ -57,93 +57,165 @@
 
 # print(data)
 
-import json
-import os
+# import json
+# import os
 
-os.system('clear')
+# os.system('clear')
 
-json_str = '{"name": "John", "age": 30}'
+# json_str = '{"name": "John", "age": 30}'
 
 
 
-filename = "test.json"
-if os.path.exists(filename):
-    with open(filename, "r") as file:
-        data = json.load(file)
-        user_request_index = len(data)
-else:
-    data = []
-    user_request_index = 0
+# filename = "test.json"
+# if os.path.exists(filename):
+#     with open(filename, "r") as file:
+#         data = json.load(file)
+#         user_request_index = len(data)
+# else:
+#     data = []
+#     user_request_index = 0
 
-user_request = {
-    "index": user_request_index,
-    "question": "This is a question"
+# user_request = {
+#     "index": user_request_index,
+#     "question": "This is a question"
+# }
+
+# chatgpt_reponse = {
+#   "choices": [
+#     {
+#       "finish_reason": "stop",
+#       "index": 0,
+#       "message": {
+#         "content": "You can add a key-value pair to a JSON object in Python by first loading the JSON into a dictionary, adding the key-value pair, and then dumping the dictionary back to a JSON string. Here's an example:\n\n```python\nimport json\n\n# JSON string\njson_str = '{\"name\": \"John\", \"age\": 30}'\n\n# Load JSON string into dictionary\ndata = json.loads(json_str)\n\n# Add key-value pair to dictionary\ndata['city'] = 'New York'\n\n# Dump dictionary to JSON string\nnew_json_str = json.dumps(data)\n\nprint(new_json_str)  # Output: {\"name\": \"John\", \"age\": 30, \"city\": \"New York\"}\n```\n\nIn this example, we first load the JSON string into a dictionary using the `json.loads()` method. We then add the key-value pair `\"city\": \"New York\"` to the dictionary. Finally, we dump the updated dictionary back to a JSON string using the `json.dumps()` method.",
+#         "role": "assistant"
+#       }
+#     }
+#   ],
+#   "created": 1678380998,
+#   "id": "chatcmpl-6sDrCasz5w4B0fTHX3MG9go9MveFc",
+#   "model": "gpt-3.5-turbo-0301",
+#   "object": "chat.completion",
+#   "usage": {
+#     "completion_tokens": 207,
+#     "prompt_tokens": 63,
+#     "total_tokens": 270
+#   }
+# }
+
+# query = {
+#     "user_request": user_request,
+#     "chatgpt_response": chatgpt_reponse,
+#     "timestamp": datetime.datetime.now()
+# }
+
+
+# new_data = {"query": query}
+# data.append(new_data)
+
+# with open(filename, "w") as file:
+#     json.dump(data, file)
+
+
+# # print(query)
+# # print(data)
+
+
+# # # Load JSON string into dictionary
+# # data = json.loads(json_str)
+
+# # # Add key-value pair to dictionary
+# # data['city'] = 'New York'
+
+# # # Dump dictionary to JSON string
+# # new_json_str = json.dumps(data)
+
+# count = len([elem for elem in query ])
+# counta = len(data)
+
+# print(data[12]['query']['user_request'])
+# response = data[user_request_index]['query']['chatgpt_response']['choices'][0]['message']['content']
+# completion_tokens = data[user_request_index]['query']['chatgpt_response']['usage']['completion_tokens']
+# prompt_tokens = data[user_request_index]['query']['chatgpt_response']['usage']['prompt_tokens']
+# total_tokens = data[user_request_index]['query']['chatgpt_response']['usage']['total_tokens']
+
+#   # "usage": {
+#   #   "completion_tokens": 207,
+#   #   "prompt_tokens": 63,
+#   #   "total_tokens": 270
+#   # }
+# print(response)
+# print(completion_tokens)
+# print(prompt_tokens)
+# # print(total_tokens)
+
+# a =input("opcion 1")
+# pause()
+# b =input("opcion 1")
+
+
+# <h1>Define the main menu options</h1>
+
+MAIN_MENU = {
+    '1': 'Option 1',
+    '2': 'Option 2',
+    '3': 'Nested Options',
+    '0': 'Exit'
 }
 
-chatgpt_reponse = {
-  "choices": [
-    {
-      "finish_reason": "stop",
-      "index": 0,
-      "message": {
-        "content": "You can add a key-value pair to a JSON object in Python by first loading the JSON into a dictionary, adding the key-value pair, and then dumping the dictionary back to a JSON string. Here's an example:\n\n```python\nimport json\n\n# JSON string\njson_str = '{\"name\": \"John\", \"age\": 30}'\n\n# Load JSON string into dictionary\ndata = json.loads(json_str)\n\n# Add key-value pair to dictionary\ndata['city'] = 'New York'\n\n# Dump dictionary to JSON string\nnew_json_str = json.dumps(data)\n\nprint(new_json_str)  # Output: {\"name\": \"John\", \"age\": 30, \"city\": \"New York\"}\n```\n\nIn this example, we first load the JSON string into a dictionary using the `json.loads()` method. We then add the key-value pair `\"city\": \"New York\"` to the dictionary. Finally, we dump the updated dictionary back to a JSON string using the `json.dumps()` method.",
-        "role": "assistant"
-      }
-    }
-  ],
-  "created": 1678380998,
-  "id": "chatcmpl-6sDrCasz5w4B0fTHX3MG9go9MveFc",
-  "model": "gpt-3.5-turbo-0301",
-  "object": "chat.completion",
-  "usage": {
-    "completion_tokens": 207,
-    "prompt_tokens": 63,
-    "total_tokens": 270
-  }
+# <h1>Define the nested menus and their options</h1>
+
+NESTED_MENU_1 = {
+    '1': 'Nested Option 1',
+    '2': 'Nested Option 2',
+    '0': 'Back'
 }
 
-query = {
-    "user_request": user_request,
-    "chatgpt_response": chatgpt_reponse,
-    "timestamp": datetime.datetime.now()
+NESTED_MENU_2 = {
+    '1': 'Nested Option 3',
+    '2': 'Nested Option 4',
+    '0': 'Back'
 }
 
+NESTED_MENUS = {
+    '1': NESTED_MENU_1,
+    '2': NESTED_MENU_2,
+    '0': 'Back'
+}
 
-new_data = {"query": query}
-data.append(new_data)
+# Define a function to display a menu and get user input</h1>
 
-with open(filename, "w") as file:
-    json.dump(data, file)
-
-
-# print(query)
-# print(data)
+def get_user_choice(menu):
+  for key, value in menu.items():
+    print(f'{key}: {value}')
+  choice = input('Please enter your choice: ')
+  return choice
 
 
-# # Load JSON string into dictionary
-# data = json.loads(json_str)
 
-# # Add key-value pair to dictionary
-# data['city'] = 'New York'
+# Define a function to execute the selected menu option
 
-# # Dump dictionary to JSON string
-# new_json_str = json.dumps(data)
+def execute_choice(choice):
+  if choice == '1':
+    print('Selected option 1.')
+  elif choice == '2':
+    print('Selected option 2.')
+  elif choice == '3':
+    nested_choice = ''
+    while nested_choice != '0':
+      nested_choice = get_user_choice(NESTED_MENUS)
+    if (nested_choice == 0):
+      return
+      execute_choice(nested_choice)
+  elif choice == '0':
+    print('Exiting')
+    exit()
+  else:
+    print('Choice...'+ choice + "/")
 
-count = len([elem for elem in query ])
-counta = len(data)
+# <h1>Use a loop to continuously display the main menu until the user selects to exit
 
-print(data[12]['query']['user_request'])
-response = data[user_request_index]['query']['chatgpt_response']['choices'][0]['message']['content']
-completion_tokens = data[user_request_index]['query']['chatgpt_response']['usage']['completion_tokens']
-prompt_tokens = data[user_request_index]['query']['chatgpt_response']['usage']['prompt_tokens']
-total_tokens = data[user_request_index]['query']['chatgpt_response']['usage']['total_tokens']
-
-  # "usage": {
-  #   "completion_tokens": 207,
-  #   "prompt_tokens": 63,
-  #   "total_tokens": 270
-  # }
-print(response)
-print(completion_tokens)
-print(prompt_tokens)
-print(total_tokens)
+if __name__ == "__main__":
+  while(True):
+    choice = get_user_choice(MAIN_MENU)
+    execute_choice(choice)
+    dir(MAIN_MENU)
